310e1531e8f782fe046968edb5a23f43665c3c99
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-06 13:25:23 +0100

    fixing shit.

---
+++ script.js
@@ -0,0 +1,16 @@
+const firstName = document.getElementById("fName");
+const lastName = document.getElementById("lName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("cpassword");
+
+const emailValidationRegex = /^\S+@\S+\.\S+$/
+const phoneValidationRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
+
+
+const checkRequiredFields = () => {
+  e.preventDefault()
+  if (condition) {
+    
+  }
+}
